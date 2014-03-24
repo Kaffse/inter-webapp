ecc1de358c5a2e0dd025a78ecfec32b03f7385cd
Keir Smith <kaffse@gmail.com>
2014-03-24 00:26:26 +0000

    added rabbit reqs

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var context = require('rabbit.js').createContext('queue.getinter.io');
+
 var app = express();
+
+var pub = context.socket('PUBLISH');
+var sub = context.socket('SUBSCRIBE');
 
 app.get('/', function(req, res){
       res.sendfile('index.html');
+});
+
+app.get('/send', function(req, res){
+    // send req.body to RMQ
 });
 
 var server = app.listen(3000, function() {
